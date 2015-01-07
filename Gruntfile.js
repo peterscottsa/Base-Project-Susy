32fd7c13b98310efc3c36396511a51d33f1f1e0c
Peter Scott <peterscott@peters-air.bluegrass.local>
2015-01-07 10:59:40 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Task configuration.
+    sass: {
+      production: {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          'build/style.css': 'sass/style.scss'
+        }
+      },
+      development: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'build/style.css' : 'sass/style.scss'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: 'sass/*.scss',
+        tasks: ['sass:development']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('default', ['sass:production']);
+  grunt.registerTask('dev', ['sass:development','watch']);
+
+};
